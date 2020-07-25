3789844e73e9048b339b2a480aca7f6c04749444
YC Xiong <YC Xiong@XXX-OOO>
2020-07-25 19:54:05 +0800

    1

--- component-parent/component-cache/src/main/java/com/xiongyc/cache/RedisUtil.java
+++ component-parent/component-cache/src/main/java/com/xiongyc/cache/RedisUtil.java
@@ -1,19 +1,31 @@
 package com.xiongyc.cache;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-@SuppressWarnings("rawtypes")
 @Component
+@SuppressWarnings(value = {"unchecked","rawtypes"})
 public class RedisUtil {
 
 	@Autowired
 	private RedisTemplate redisTemplate;
 
-	@SuppressWarnings("unchecked")
 	public Long getSeq(String tableName, long l) {
 		return redisTemplate.opsForValue().increment(tableName, l);
 	}
 
+	public void set(Object key, Object opsForValue, long timeout, TimeUnit unit) {
+		redisTemplate.opsForValue().set(key, opsForValue, timeout, unit);
+	}
+	
+	public void set(Object key, Object value) {
+		redisTemplate.opsForValue().set(key, value);
+	}
+	
+	public Object get(String key) {
+		return redisTemplate.opsForValue().get(key);
+	}
 }
